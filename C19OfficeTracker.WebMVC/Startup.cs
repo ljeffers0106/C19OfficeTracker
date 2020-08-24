a1a8116bc915d0fca7d9f30f0df2468931219d73
Lisa Jeffers <lisajeffers0106@gmail.com>
2020-08-24 09:35:05 -0400

    FirstCommit

---
+++ C19OfficeTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(C19OfficeTracker.WebMVC.Startup))]
+namespace C19OfficeTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
